7d4a7990faa95948646c4b1536b8376f86457b60
Niels Mündler <n.muendler@posteo.de>
2023-02-03 15:16:17 +0100

    Fix copyright notice

--- uplc/__init__.py
+++ uplc/__init__.py
@@ -19,6 +19,6 @@ VERSION = (0, 5, 1)
 __version__ = ".".join([str(i) for i in VERSION])
 __author__ = "nielstron"
 __author_email__ = "n.muendler@web.de"
-__copyright__ = "Copyright (C) 2019 nielstron"
+__copyright__ = "Copyright (C) 2023 nielstron"
 __license__ = "MIT"
 __url__ = "https://github.com/imperatorlang/uplc"
