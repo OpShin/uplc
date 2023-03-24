50cde7466a74a445c4a4b1b4a239058b4ba3cc93
Niels Mündler <n.muendler@posteo.de>
2023-03-24 09:21:10 +0100

    Ensure pos integers are u64 for aiken

--- uplc/tests/test_hypothesis.py
+++ uplc/tests/test_hypothesis.py
@@ -18,7 +18,7 @@ def frozenlist(l):
     return l
 
 
-pos_int = hst.integers(min_value=0, max_value=1000000000)
+pos_int = hst.integers(min_value=0, max_value=2**64 - 1)
 
 
 uplc_data_integer = hst.builds(PlutusInteger, hst.integers())
